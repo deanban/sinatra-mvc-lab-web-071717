d493d164ecf299fe27f759037f7b3db1f62d55f5
deanban <pbp2005@gmail.com>
2017-08-09 11:41:36 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get '/' do 
+		erb :user_input
+	end
+
+	post '/piglatinize' do 
+		@str = params[:user_phrase]
+
+
+		erb :piglatinizer
+	end
+
 end
